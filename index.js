31ede7ffbb3de996b99289058c07a48a9d52154f
kehindeBankole <bankolek1@gmail.com>
2020-10-06 16:48:18 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+
 document.getElementById("open").addEventListener("click", () => {
  document.getElementById("navlink").style.right = "0";
-//gsap.to(".navlist", {right: '0', duration: 0.2, ease: 'elastic'});
 });
 document.getElementById("close").addEventListener("click", () => {
   document.getElementById("navlink").style.right = "-30rem";
+});
+const boxes = gsap.utils.toArray('.box');
+gsap.registerPlugin(ScrollTrigger)
+boxes.forEach((box, i) => {
+  const anim = gsap.fromTo(box, {autoAlpha: 0, y: 10}, {duration: 1, autoAlpha: 1, y: 0});
+  ScrollTrigger.create({
+    trigger: box,
+    animation: anim,
+    toggleActions: 'play none restart none',
+    once: false,
+  });
 });
