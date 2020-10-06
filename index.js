1712f755bdaa406b3b0be117b5772320fe0f5172
kehindeBankole <bankolek1@gmail.com>
2020-10-06 13:43:36 +0200

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.getElementById("open").addEventListener("click", () => {
-  document.getElementById("navlink").style.right = "0";
+ document.getElementById("navlink").style.right = "0";
+//gsap.to(".navlist", {right: '0', duration: 0.2, ease: 'elastic'});
 });
 document.getElementById("close").addEventListener("click", () => {
   document.getElementById("navlink").style.right = "-30rem";
