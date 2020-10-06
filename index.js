3f493ecf940e3677b159b936622486980f6fba13
kehindeBankole <bankolek1@gmail.com>
2020-10-06 11:31:38 +0200

    changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-document.getElementById("open").addEventListener("click" , ()=>{
-    document.getElementById("navlink").style.right="50%"
-})
+document.getElementById("open").addEventListener("click", () => {
+  document.getElementById("navlink").style.right = "0";
+});
+document.getElementById("close").addEventListener("click", () => {
+  document.getElementById("navlink").style.right = "-30rem";
+});
